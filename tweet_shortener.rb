772411dc43eef0fdb910860e14bf530f6b410c23
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-24 02:32:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 dictionary={
-"hello" becomes 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
+"hello" => 'hi',
+"to, two, too" => '2', 
+"for, four"=> '4',
+'be' => 'b',
+'you'=> 'u',
+"at" => "@" ,
+"and" => "&"
 }
